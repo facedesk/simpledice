ee19b6cc22709af66df766fd0cf8fd1a01d241e3
face desk <dgold@sjnma.org>
2016-12-21 09:42:05 -0600

    added sensor for rolling, working on color coding + one number is out of range

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from sense_hat import SenseHat
 from random import randint
 
-
 sense = SenseHat()
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0,255)
+red  = (255,0,0)
+blue=(0,0,255)
+green=(0,255,0)
 min=1
-max=6
-num = randint(min,max)
+max =6
 
+while True:
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z= acceleration['z']
 
+    x = round(x,0)
+    y = round(y,0)
+    z = round(z,0)
 
-if((max/num) >= 3.0):
-  sense.show_message(str(num), text_colour = blue)
-elif((max/num) >= 2.0):
-  sense.show_message(str(num), text_colour = green)
-else:
-  sense.show_message(str(num), text_colour = red)
+#    print("x={0}, y= {1}, z = {2}".format(x,y,z))
+    if(x>1 or y>1 or z>1):
+
+        num = randint(min,max)
+	print(str(num) +" > "+str(max/num))
+        if (max/num)<=(1):
+            sense.show_message(str(num),text_colour=blue)
+        elif (max/num)<=(2):
+            sense.show_message(str(num),text_colour=green)
+        elif ((max/num)<= 3):
+            sense.show_message(str(num),text_colour=red)
+	else:
+	    sense.show_message("invalid "+str(num),text_colour=red)
